972f54c7ab0cfb5ba6155e519db271201ee41870
Collin Ahn <collinahn@hufs.ac.kr>
2024-05-07 22:32:47 +0900

    추가될 모델 정의

---
+++ model/ingredients.py
@@ -0,0 +1,34 @@
+'''
+Ingredients 클래스 및 IngredientsInput 클래스 정의
+
+Class 상세를 참고하여 dataclass로 작성
+
+딕셔너리 형태로 바꿔주는 메소드가 공통적으로 들어감(ToDict 클래스 상속)
+'''
+from dataclasses import dataclass, asdict, field, is_dataclass
+
+
+class ToDict:
+    def to_dict(self):
+        return {
+            key: value
+            for key, value in asdict(self).items()
+            if value
+        }
+        
+
+@dataclass
+class Ingredients(ToDict):
+    '''
+    재료 클래스
+    '''
+    ...
+    
+    
+@dataclass
+class IngredientsInput(Ingredients, ToDict):
+    '''
+    재료 입력 클래스
+    유저가 재료를 추가할 때마다 생성됨
+    '''
+    ...
---
+++ model/recipe.py
@@ -0,0 +1,19 @@
+'''
+Recipe 클래스 정의
+
+Class 상세를 참고하여 dataclass로 작성
+
+딕셔너리 형태로 바꿔주는 메소드가 공통적으로 들어감(ToDict 클래스 상속)
+'''
+from dataclasses import dataclass, asdict, field, is_dataclass
+from model.ingredients import ToDict
+        
+
+@dataclass
+class Recipe(ToDict):
+    '''
+    Recipe 클래스
+    '''
+    ...
+    
+    
