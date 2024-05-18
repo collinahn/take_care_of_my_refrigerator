2838b7e3c057800cceedeebf209e7f991879fa3b
hhhhhyun <woojoo910@hufs.ac.kr>
2024-05-18 10:11:54 +0900

    search 수정, recipe 추가

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,22 +1,29 @@
 function activateTab(evt, tabName) {
-    // Declare all variables
     var i, tablinks, contentBoxes;
 
-    // Get all elements with class="tab-link" and remove the class "active"
     tablinks = document.getElementsByClassName("tab-link");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Hide all content boxes
-    contentBoxes = document.getElementsByClassName("content-box");
+    contentBoxes = document.getElementsByClassName("tab-content");
     for (i = 0; i < contentBoxes.length; i++) {
         contentBoxes[i].style.display = "none";
     }
 
-    // Add an "active" class to the button that opened the tab
     evt.currentTarget.className += " active";
 
-    // Show the current tab
     document.getElementById(tabName).style.display = "block";
+}
+
+function toggleFavorite(button) {
+    if (button.innerHTML === "☆") {
+        button.innerHTML = "★";
+    } else {
+        button.innerHTML = "☆";
+    }
+}
+
+window.onload = function() {
+    document.querySelector('.tab-link[onclick="activateTab(event, \'search\')"]').click();
 }
