9939836929b88012554f021653fb2350be27df0f
Collin Ahn <collinahn@hufs.ac.kr>
2024-05-28 01:53:28 +0900

    add refrigerator API endpoints in views/api/refrigerator.py
    식재료 추가 수정 삭제 api

--- views/api/refrigerator.py
+++ views/api/refrigerator.py
@@ -1,15 +1,111 @@
-import json
-import time
 from flask import Blueprint, request
 from pymongo.errors import PyMongoError
+from bson import ObjectId
 
 from db.mongo import db_users
-from utils.pwa_push import PushMsgFormat, PushSettings
-from utils.celery_utils import  bulk_write_to_collection, push_history_to_be_recorded
 from utils.response import incorrect_data_response, server_error, success_response
-from tasks.push.worker import send_push_notification
 from utils.logger import get_logger
 
 bp_refrigerator = Blueprint('refrigerator', __name__, url_prefix='/api/refrigerator')
 log = get_logger()
 
+@bp_refrigerator.post('/add/')
+def add_ingredients():
+    try:
+        data: dict = request.get_json(silent=True)
+        if not data:
+            return incorrect_data_response('입력하신 정보가 올바르지 않습니다.'), 400
+        endpoint = data.get('endpoint')
+        ingredients = data.get('ingredient')
+        if not endpoint or not ingredients:
+            return incorrect_data_response()
+        
+        update_res = db_users.update_one(
+            {'sub.endpoint': endpoint}, 
+            {
+                '$push': {
+                    'ingredients': {
+                        '$each': [{
+                            'id': str(ObjectId()),
+                            **ingredients,
+                            }],
+                    }
+                }
+            }
+        )
+        if update_res.matched_count == 0:
+            return incorrect_data_response('존재하지 않는 사용자입니다.'), 400
+        
+        return success_response("추가되었습니다.")
+    except PyMongoError as e:
+        log.error(e)
+        return server_error(), 500
+    except Exception as e:
+        log.error(e)
+        return server_error(), 500
+    
+    
+@bp_refrigerator.post('/update/')
+def update_ingredients():
+    try:
+        data: dict = request.get_json(silent=True)
+        if not data:
+            return incorrect_data_response('입력하신 정보가 올바르지 않습니다.'), 400
+        endpoint = data.get('endpoint')
+        ingredient: dict = data.get('ingredient', {})
+        ingredient_id: str = ingredient.get('ingredient_id', '')
+        if not endpoint or not ingredient_id or not ingredient:
+            return incorrect_data_response('입력하신 정보가 올바르지 않습니다.'), 400
+        
+        update_res = db_users.update_one(
+            {'sub.endpoint': endpoint, 'ingredients.id': ingredient_id}, 
+            {
+                '$set': {
+                    f'ingredients.$[elem].{key}': value 
+                    for key, value in ingredient.items() 
+                    if key != 'id'
+                }
+            },
+            array_filters=[
+                {'ingredient.id': ingredient_id}
+            ]
+        )
+        if update_res.matched_count == 0:
+            return incorrect_data_response('존재하지 않는 사용자이거나 재료가 없습니다.'), 400
+        
+        return success_response("수정되었습니다.")
+    except PyMongoError as e:
+        log.error(e)
+        return server_error(), 500
+    except Exception as e:
+        log.error(e)
+        return server_error(), 500
+    
+@bp_refrigerator.post('/delete/')
+def delete_ingredients():
+    try:
+        endpoint = request.args.get('endpoint')
+        ingredient_id = request.args.get('id')
+        if not endpoint or not ingredient_id:
+            return incorrect_data_response('입력하신 정보가 올바르지 않습니다.'), 400
+        
+        update_res = db_users.update_one(
+            {'sub.endpoint': endpoint}, 
+            {
+                '$pull': {
+                    'ingredients': {
+                        'id': ingredient_id
+                    }
+                }
+            }
+        )
+        if update_res.matched_count == 0:
+            return incorrect_data_response('존재하지 않는 사용자이거나 재료가 없습니다.'), 400
+        
+        return success_response("삭제되었습니다.")
+    except PyMongoError as e:
+        log.error(e)
+        return server_error(), 500
+    except Exception as e:
+        log.error(e)
+        return server_error(), 500
